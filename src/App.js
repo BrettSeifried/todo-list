04025e4849eac090ec9674e3fb6fb251629621df
Brett Seifried <seifriedbrett@gmail.com>
2022-01-10 17:44:25 -0800

    set up App.js and users.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useState } from 'react';
+import { getUser, logout } from './services/users.js';
+import { Auth } from 'react';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(getUser());
+  const logoutUser = async () => {
+    await logout();
+    setCurrentUser(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <p>test home page</p>
+            {currentUser && (
+              <>
+                <h1> Your to-do List</h1>
+                <button onClick={logout}>Logout</button>
+              </>
+            )}
+            {!currentUser && <Auth setCurrentUser={setCurrentUser} />}
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </main>
   );
 }
 
